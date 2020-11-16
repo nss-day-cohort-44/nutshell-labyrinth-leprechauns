3d5dd3e148e852a684835c4497354c6139f25654
negative1skunk@gmail.com <negative1skunk@gmail.com>
2020-11-16 11:00:28 -0600

    updated comments

--- src/scripts/auth/InitialRender.js
+++ src/scripts/auth/InitialRender.js
@@ -2,17 +2,24 @@ import { LoginForm } from "./LoginForm.js"
 import { RegisterForm } from "./RegisterForm.js"
 import { Nutshell } from "../Nutshell.js"
 
+// On page load this function determines if an activeUser is set and if so it renders the main App and if not it renders the login and reg form
 export const InitialRender = () => {
   const mainBody = document.querySelector(".mainBody")
   const auth = document.querySelector(".auth")
-  if (!window.sessionStorage.activeUser) {
+
+  // If no activeUser, render loging and Registration form
+
+  if (!sessionStorage.activeUser) {
     mainBody.classList.add(".hide")
     auth.classList.remove(".hide")
     LoginForm()
     RegisterForm()
   }
 
-  if (window.sessionStorage.activeUser) {
+  // If activeUser, render main App
+
+  if (sessionStorage.activeUser) {
+    console.log(sessionStorage)
     mainBody.classList.remove(".hide")
     auth.classList.add(".hide")
     Nutshell()
