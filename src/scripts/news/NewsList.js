2c785d8a145c7a21cf31a69aa50c8d7fa2236ad7
Travis Milner <travismilner1@gmail.com>
2020-11-17 14:51:44 -0600

    added an array filter to get the news from friends to render

--- src/scripts/news/NewsList.js
+++ src/scripts/news/NewsList.js
@@ -1,6 +1,7 @@
 import { renderNewsButton } from "./NewsButton.js"
 import { newsArticleCard } from "./NewsCard.js"
 import { deleteNews, getNews, useNews, getNewsArrayByUser } from "./NewsDataProvider.js"
+import { useFriends, getFriendArrayByUser, getFriends } from "../friends/FriendProvider.js"
 
 const eventHub = document.querySelector(".container")
 
@@ -9,9 +10,18 @@ eventHub.addEventListener("newsStateChanged", () =>NewsList())
 export const NewsList = () =>{
     
     getNews()
+    .then(getFriends)
     .then(() =>{
         const allArticles = useNews()
-        render(allArticles)
+        // debugger
+        const friendsArray = getFriendArrayByUser(+sessionStorage.getItem("activeUser"))
+        const filteredArticles = allArticles.filter(fr => {
+            if(fr.userId === +sessionStorage.getItem("activeUser")) {
+                return true 
+            } else 
+            {return friendsArray.find(f => f.followingId === fr.userId)}
+        })
+        render(filteredArticles)
     })
 }
 
