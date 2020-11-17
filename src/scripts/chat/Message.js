975d57488aef5d94fe97693a94c9e73b18ca2c3e
negative1skunk@gmail.com <negative1skunk@gmail.com>
2020-11-17 11:59:18 -0600

    delete btn werks

--- src/scripts/chat/Message.js
+++ src/scripts/chat/Message.js
@@ -1,3 +1,5 @@
 export const Message = (userObj, messageObj) => {
-  return `<p>${userObj.username}: ${messageObj.message}</p>`
+  return `<p>${userObj.username}: ${messageObj.message}</p>
+  <button id="deleteMessage--${messageObj.id}">Delete</button>
+  `
 }
