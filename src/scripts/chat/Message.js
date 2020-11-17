20df17b43b07f44b66d74463fc3ce67688f87b11
negative1skunk@gmail.com <negative1skunk@gmail.com>
2020-11-17 13:54:41 -0600

    comment

--- src/scripts/chat/Message.js
+++ src/scripts/chat/Message.js
@@ -1,3 +1,5 @@
+// Silas, generates the messages
+
 export const Message = (userObj, messageObj) => {
   return `
   <p>${userObj.username}: ${messageObj.message} <button id="deleteMessage--${messageObj.id}" class="btn">delete</button></p>
