3dca61c993a547cfe74d0a0e4d626d20b05c1349
dalamcd <dalamcd@gmail.com>
2020-11-18 10:18:54 -0600

    Events are sorted chronologically and the first event is bolded

--- src/scripts/events/EventList.js
+++ src/scripts/events/EventList.js
@@ -3,7 +3,7 @@
 */
 
 import { getEventsArrayByUser, getEvents, getEventById } from "./EventProvider.js";
-import { EventCard } from "./Event.js";
+import { EventCard, EventCardFirst } from "./Event.js";
 import { eventWeather } from "../weather/WeatherSelect.js";
 
 
@@ -14,19 +14,45 @@ export const EventList = () => {
     const user = parseInt(sessionStorage.getItem("activeUser"));
 
     const evArray = getEventsArrayByUser(user);
+    evArray.sort((evOne, evTwo) => {
+        const [yearOne, dayOne, monthOne] = evOne.eventDate.split("-")
+        const [yearTwo, dayTwo, monthTwo] = evTwo.eventDate.split("-")
+        return (parseInt(yearOne) + parseInt(dayOne) + parseInt(monthOne)) -
+            (parseInt(yearTwo) + parseInt(dayTwo) + parseInt(monthTwo));
+    })
+
+    const [month, date, year] = new Date().toLocaleDateString("en-US").split("/")
+    evArray.forEach(ev => {
+        const [evYear, evDay, evMonth] = ev.eventDate.split("-")
+
+        if ((parseInt(evYear) + parseInt(evDay) + parseInt(evMonth)) <
+            (parseInt(month) + parseInt(date) + parseInt(year))) {
+            eventHub.dispatchEvent(new CustomEvent("deleteEventEvent", {
+                detail: {
+                    eventId: ev.id
+                }
+            }));
+        }
+    })
+
     let htmlRep = "<h2>Event List</h2>"
-    htmlRep += evArray.map(ev => `${EventCard(ev)}${AddDeleteButton(ev)}${AddWeatherButton(ev)}`).join("");
+    htmlRep += evArray.map((ev, i) => {
+        if (i === 0)
+            return `${EventCardFirst(ev)}${AddDeleteButton(ev)}${AddWeatherButton(ev)}`
+        else
+            return `${EventCard(ev)}${AddDeleteButton(ev)}${AddWeatherButton(ev)}`
+    }).join("");
     contentTarget.innerHTML = htmlRep;
 }
 
 eventHub.addEventListener("eventListStateChanged", e => {
     getEvents()
-    .then(EventList);
+        .then(EventList);
 })
 
 const AddDeleteButton = ev => {
     // Only add a delete button if the active user created this event.
-    if(ev.userId === +sessionStorage.getItem("activeUser"))
+    if (ev.userId === +sessionStorage.getItem("activeUser"))
         return `<button id="deleteEvent--${ev.id}">Delete</button>`;
     else
         return ``;
@@ -37,7 +63,7 @@ const AddWeatherButton = ev => {
 }
 
 eventHub.addEventListener("click", e => {
-    if(e.target.id.startsWith("deleteEvent")) {
+    if (e.target.id.startsWith("deleteEvent")) {
         const [temp, eventId] = e.target.id.split("--");
         eventHub.dispatchEvent(new CustomEvent("deleteEventEvent", {
             detail: {
@@ -45,7 +71,7 @@ eventHub.addEventListener("click", e => {
             }
         }))
     }
-    if(e.target.id.startsWith("eventWeather")) {
+    if (e.target.id.startsWith("eventWeather")) {
         const [temp, eventId] = e.target.id.split("--");
         const ev = getEventById(parseInt(eventId));
         eventWeather(ev.eventCity, ev.eventState);
