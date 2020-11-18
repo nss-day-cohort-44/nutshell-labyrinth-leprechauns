fe65681865d9eb80640891c7a0b984815293c1d0
negative1skunk@gmail.com <negative1skunk@gmail.com>
2020-11-18 11:20:54 -0600

    friend name in chat is clickable if you are not friends and adds friend to friend list

--- src/scripts/chat/Message.js
+++ src/scripts/chat/Message.js
@@ -13,25 +13,23 @@ const AddDeleteButton = (messageObj) => {
 }
 
 const addNameButton = (messageObj, userObj) => {
-  return getFriends().then(() => {
-    // relationship objects
-    const relationships = useFriends()
-
-    // relationships for current user
-    const relatedFriends = relationships.filter((relationship) => {
-      return relationship.userId === +sessionStorage.activeUser
-    })
-    // finds determines if the message was written by a friend
-    const matchingFriend = relatedFriends.find((friend) => {
-      return friend.followingId === messageObj.userId
-    })
-
-    if (!matchingFriend) {
-      return `<p>${userObj.username}: ${messageObj.message} ${AddDeleteButton(messageObj)}`
-    } else {
-      return `<button><p>${userObj.username}</button>: ${messageObj.message} ${AddDeleteButton(
-        messageObj
-      )}`
-    }
+  const relationships = useFriends()
+  // relationships for current user
+  const relatedFriends = relationships.filter((relationship) => {
+    return relationship.userId === +sessionStorage.activeUser
   })
+  // finds determines if the message was written by a friend
+  const matchingFriend = relatedFriends.find((friend) => {
+    return friend.followingId === messageObj.userId
+  })
+
+  if (matchingFriend) {
+    console.log("UO", userObj)
+    console.log("MO", messageObj)
+    return `<p>${userObj.username}: ${messageObj.message} ${AddDeleteButton(messageObj)}</p>`
+  } else {
+    return `<p><button  id="addFriendFromMessage--${messageObj.userId}" class="btn"">${
+      userObj.username
+    }</button>: ${messageObj.message} ${AddDeleteButton(messageObj)}</p>`
+  }
 }
