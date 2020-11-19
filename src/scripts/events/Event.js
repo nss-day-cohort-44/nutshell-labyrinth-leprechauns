beda254b825b192744efc3d63e3cca4739edaa35
negative1skunk@gmail.com <negative1skunk@gmail.com>
2020-11-19 10:03:12 -0600

    edit function and button added to card

--- src/scripts/events/Event.js
+++ src/scripts/events/Event.js
@@ -2,16 +2,16 @@
     Purpose: Draw an individual event card.
 */
 
-export const EventCard = ev => {
-    if(ev.userId === parseInt(sessionStorage.getItem('activeUser')))
-        return `<div class="eventCard"><p>${ev.name}</p> <p>${ev.eventCity}, ${ev.eventState}</p> <p>${ev.eventDate}</p></div>`
-    else
-        return `<div class="eventCardFriend"><p>${ev.name}</p> <p>${ev.eventCity}, ${ev.eventState}</p> <p>${ev.eventDate}</p></div>`
+export const EventCard = (ev) => {
+  if (ev.userId === parseInt(sessionStorage.getItem("activeUser")))
+    return `<div class="eventCard"><p>${ev.name}</p> <p>${ev.eventCity}, ${ev.eventState}</p> <p>${ev.eventDate}</p><button class="editEvent--${ev.id}">edit</button></div>`
+  else
+    return `<div class="eventCardFriend"><p>${ev.name}</p> <p>${ev.eventCity}, ${ev.eventState}</p> <p>${ev.eventDate}</p></div>`
 }
 
-export const EventCardFirst = ev => {
-    if(ev.userId === parseInt(sessionStorage.getItem('activeUser')))
-        return `<div class="eventCard nonOffensive"><p><b>${ev.name}</p> <p>${ev.eventCity}, ${ev.eventState}</p> <p>${ev.eventDate}</p></b></div>`
-    else
-        return `<div class="eventCardFriend nonOffensive"><p><b>${ev.name}</p> <p>${ev.eventCity}, ${ev.eventState}</p> <p>${ev.eventDate}</p></b></div>`
+export const EventCardFirst = (ev) => {
+  if (ev.userId === parseInt(sessionStorage.getItem("activeUser")))
+    return `<div class="eventCard nonOffensive"><p><b>${ev.name}</p> <p>${ev.eventCity}, ${ev.eventState}</p> <p>${ev.eventDate}</p></b><button class="editEvent--${ev.id}">edit</button></div>`
+  else
+    return `<div class="eventCardFriend nonOffensive"><p><b>${ev.name}</p> <p>${ev.eventCity}, ${ev.eventState}</p> <p>${ev.eventDate}</p></b></div>`
 }
